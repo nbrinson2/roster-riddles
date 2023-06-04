5b6b8d6a4b05d94041898184ad5c6a186ae494e4
nbrinson2 <nickbrinson3@gmail.com>
2023-06-04 14:36:55 -0400

    Add folder structure for backend

---
+++ src/main/java/com/rosterriddles/rosterriddles/RosterriddlesApplication.java
@@ -0,0 +1,13 @@
+package com.rosterriddles.rosterriddles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosterriddlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosterriddlesApplication.class, args);
+	}
+
+}
